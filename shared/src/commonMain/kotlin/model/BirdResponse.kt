6d2f709ccdd01efd91861770f13a16e0d03221d4
Raaj Kanchan <iamraajkanchan@gmail.com>
2023-08-05 00:52:00 +0530

    @SerialName annotation is added for every property in BirdResponse

--- shared/src/commonMain/kotlin/model/BirdResponse.kt
+++ shared/src/commonMain/kotlin/model/BirdResponse.kt
@@ -1,11 +1,15 @@
 package model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class BirdResponse(
+	@SerialName("path")
 	val path: String? = null,
+	@SerialName("author")
 	val author: String? = null,
+	@SerialName("category")
 	val category: String? = null
 )
 
